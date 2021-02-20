e42d83db28bd26f2ca2852d76d69d1c60e1f052f
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2021-02-20 13:45:15 -0400

    chore: cria testes unitários

---
+++ MoaAnimeLALGCompliler/Compilador/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Compilador
+{
+    public struct Token
+    {
+        public Token(string valor, TipoToken tipo)
+        {
+            Valor = valor;
+            Tipo = tipo;
+        }
+
+        public string Valor { get; set; }
+        public TipoToken Tipo { get; set; }
+    }
+}

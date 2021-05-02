b39beb7e562279b362e08e68e7511b3068dd1741
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2021-05-02 12:51:22 -0400

    usa helper pra peger string de teste

--- MoaAnimeLALGCompliler/Testes/Sintatico.cs
+++ MoaAnimeLALGCompliler/Testes/Sintatico.cs
@@ -11,56 +11,8 @@ namespace Testes
         public void EntradaValida()
         {
             //Arrange
-            //var entrada = System.IO.File.ReadAllText(@"Testes/EntradaCorreta.txt");
-            //System.IO.File.WriteAllText("entrada.txt", "if numero > 1\n123abc");
-            var analisadorLexico = new AnalisadorLexico(@"
-                  program teste
-
-                  /* declaracao de variaveis */
-                  var a,b,c: integer;
-                  var d,e,f: real;
-                  var g,h : integer;
-
-                  /* declaracao de procedimentos */
-                  procedure um (a, g: real; d, c: integer)
-                    var h, i, j: real;
-                    var l: integer
-                  begin
-                    h := 2.0;
-                    a := g + 3.4 / h;
-                    l := c - d * 2;
-                    if (c+d)>=5 then
-                      write(a)
-                    else
-                      write(l)
-                    $
-                  end;
-
-                  procedure dois (j: integer; k: real; l: integer)
-                    var cont,quant: integer
-                  begin
-                    read(quant);
-                    while cont <= quant do
-                       write(cont)
-                    $;
-                    l := l + j + cont;
-                    write(k);
-                    write(l)
-                  end
-
-                  /*  corpo * principal / */
-
-                  begin
-                  read(e); {real}
-                  read(f); {real}
-                  read(g); {inteiro}
-                  read(h); { inteiro} 
-                  d := e/f; {real}
-                  dois(h;d;h);
-                  um(f;e;g;h)         {real,real,inteiro,inteiro}
-                  {aqui termina o programa}
-                  end.");
-            
+            var analisadorLexico = new AnalisadorLexico(Helper.GetExemploCorretoLalg());
+                  
             analisadorLexico.Analisar();
             var analisadorSintatico = new AnalisadorSintatico(analisadorLexico.Tokens);
 

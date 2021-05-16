7ad3d4d997d13f045d9f4c372e5061dbd315c2be
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2021-05-16 11:04:26 -0400

    melhoraq mensagens proigram.cs

--- MoaAnimeLALGCompliler/Compilador/Program.cs
+++ MoaAnimeLALGCompliler/Compilador/Program.cs
@@ -7,11 +7,14 @@ namespace Compilador
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Inicio");
+            Console.WriteLine("Inicio\nA eentrada será lida do aquivo Entrada.txt");
             var analisadorLexico = new AnalisadorLexico(File.ReadAllText("Entrada.txt"));
             analisadorLexico.Analisar();
+            Console.WriteLine("Analise léxica completa...");
             var analisadorSintatico = new AnalisadorSintatico(analisadorLexico.Tokens);
             analisadorSintatico.Analisar();
+            Console.WriteLine("Analise sintática completa...");
+            Console.WriteLine("Analise semantica completa...");
             Console.WriteLine("Acabou");
         }
     }
